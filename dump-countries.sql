ddc2e545a29801184066b149b7f7becf531ec96c
Lucas R.Pasquetto <lucas@blitsoft.com.br>
2017-12-03 15:04:52 -0300

    pasta com SQLs disponiveis

--- dump-countries.sql
+++
@@ -1,2 +0,0 @@
-
-INSERT INTO `countries` (`id`,`code_iso2`,`code_iso3`,`name`,`code_phone`,`lang`,`created_at`,`updated_at`) VALUES (1,'BR','BRA','Brazil','+55','pt-br','2017-12-03 17:20:11','2017-12-03 17:20:11');
