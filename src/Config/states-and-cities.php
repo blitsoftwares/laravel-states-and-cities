5eefbef629dac6f3dbb2b177a1a13dd704330626
Lucas R.Pasquetto <lucas@blitsoft.com.br>
2017-12-03 17:07:53 -0300

    Adicionado na config cidade padrao

--- src/Config/states-and-cities.php
+++ src/Config/states-and-cities.php
@@ -3,20 +3,31 @@
 return [
 
     /*
+     * In these settings you can set the default country,
+     * state and city for the selected fields.
+     */
+
+    /*
      * SET DEFAULT COUNTRY USING PRIMARY_KEY
      * SEE # IN TABLE LIST
+     *
+     * default is 1 - Brazil
      */
     "default-country" => 1,
 
     /*
      * SET DEFAULT STATE USING PRIMARY_KEY
      * SEE # IN TABLE LIST
+     *
+     * default is 12 - Mato Grosso do Sul
      */
     "default-state" => 12,
 
     /*
      * SET DEFAULT CITY USING PRIMARY_KEY
      * SEE # IN TABLE LIST
+     *
+     * default is 5123 - Campo Grande
      */
     "default-city" => 5123,
 
