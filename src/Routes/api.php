5ac88fad1b37e866cd2bff6197c5f8782919e376
Lucas Pasquetto <lucas@blitsoft.com.br>
2018-02-24 07:38:12 -0400

    API ajuste de resources

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -3,11 +3,14 @@
  *  API V1 ROUTES
  */
 Route::prefix('api/v1')->group(function (){
-    Route::resource('countries','CountriesApiV1Controller');
-    Route::get('countries/{country}/states','StatesApiV1Controller@index');
 
-    Route::resource('states','StatesApiV1Controller');
+    Route::resources([
+        'countries'=>'CountriesApiV1Controller',
+        'states'=>'StatesApiV1Controller',
+        'cities'=>'CitiesApiV1Controller'
+    ]);
+
+    Route::get('countries/{country}/states','StatesApiV1Controller@index');
     Route::get('states/{state}/cities','CitiesApiV1Controller@index');
 
-    Route::resource('cities','CitiesApiV1Controller');
 });
